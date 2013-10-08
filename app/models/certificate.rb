8c0c236485f826cb045e001920b5030cb3367974
akagisho <akagisho@gmail.com>
2013-10-09 00:24:08 +0900

    Add validation

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,3 +1,21 @@
 class Certificate < ActiveRecord::Base
   attr_accessible :checked_at, :common_name, :expired_at, :ipv4addr, :issuer, :note, :organization, :port
+
+  validates :common_name,
+    :presence => true,
+    :length => { :maximum => 255 },
+    :format => { :with => /^[a-z0-9\-\.]+\.[a-z]+$/i }
+  validates :ipv4addr,
+    :length => { :maximum => 15 },
+    :format => {
+      :allow_blank => true,
+      :with => /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/
+    }
+  validates :port,
+    :numericality => {
+      :allow_blank => true,
+      :only_integer => true,
+      :greater_than => 0,
+      :less_than => 65536
+    }
 end
